fc6bca539341e50ecd6e93df54f98982669a7731
etiennedi <etienne@dilocker.de>
2018-01-08 20:10:46 +0100

    add jest and simple cpu parser

---
+++ index.js
@@ -0,0 +1,13 @@
+function cpuParser(input) {
+  const milliMatch = input.match(/^([0-9]+)m$/)
+  if(milliMatch) {
+    return milliMatch[1] / 1000
+  }
+
+  return parseFloat(input)
+}
+
+module.exports = {
+  cpuParser,
+
+}
---
+++ index.test.js
@@ -0,0 +1,19 @@
+const { cpuParser } = require('./index')
+
+describe('a cpu parser', () => {
+
+  // pattern is [description, input, output]
+  const tests = [
+    ['parses full numbers', '1', 1],
+    ['parses floats', '1.5', 1.5],
+    ['parses strings with milli (m) unit (whole number)', '1000m', 1],
+    ['parses strings with milli (m) unit (decimal number)', '1300m', 1.3],
+    ['parses strings with milli (m) unit (< 1)', '300m', 0.3],
+  ]
+
+  tests.map( t =>( it(t[0], () => {
+    expect( cpuParser(t[1])).toEqual(t[2])
+  })))
+
+
+})
